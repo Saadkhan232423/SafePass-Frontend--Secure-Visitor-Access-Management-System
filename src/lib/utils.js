82ad9e7b210422a9acef9eaa4eef0982e4778650
Saadkhan232423 <151642026+Saadkhan232423@users.noreply.github.com>
2025-12-30 16:37:54 +0500

    Update utils.js

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -14,7 +14,6 @@ export function generateGatePass() {
   }
   return result;
 }
-
 export function generateCNIC() {
   return `${Math.floor(10000 + Math.random() * 90000)}-${Math.floor(1000000 + Math.random() * 9000000)}-${Math.floor(1 + Math.random() * 9)}`;
 }
